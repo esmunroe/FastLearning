e208e89df918b8a0015ca9a854afc64a2303c5a8
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-11 16:35:25 -0500

    Project Creation

---
+++ src/main/java/com/group12/FastLeaning/FastLeaningApplication.java
@@ -0,0 +1,13 @@
+package com.group12.FastLeaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastLeaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastLeaningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group12/FastLeaning/FastLeaningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group12.FastLeaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastLeaningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
