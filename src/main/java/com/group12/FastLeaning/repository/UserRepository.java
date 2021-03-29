3c45a96e179dff2e3360846f3a1039cb661928cb
Elijah Munroe <emunroe@uncc.edu>
2021-03-29 00:53:12 -0400

    User Service & Repository to get and save new users

---
+++ src/main/java/com/group12/FastLeaning/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.group12.FastLeaning.repository;
+
+import com.group12.FastLeaning.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
