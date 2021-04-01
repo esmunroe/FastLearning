0ad18ae0f739ef9fac668894c391c60b2169ae91
Elijah Munroe <emunroe@uncc.edu>
2021-04-01 04:28:38 -0400

    Creating Sample Courses

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
 MERGE INTO ROLE VALUES (1, 'USER'), (2, 'MODERATOR'), (3, 'ADMIN');
+
+MERGE INTO COURSE VALUES (1, 'Bojan Cukic','Software Development Projects','ITSC 4155'),
+(2, 'Min Shin','Computer Vision','ITCS 4152'),
+(3, 'Sterling McLeod','Intelligent Robotics','ITCS 4151'),
+(4, 'Jason Giersch','Topics in American Politics or Public Administration','POLS 3010');
